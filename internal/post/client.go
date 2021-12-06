330edad49bfe6b89f4d37f31a512f65fa4edbf06
jxlwqq <jxlwqq@gmail.com>
2021-12-06 11:56:55 +0800

    grpc dial with context

--- internal/post/client.go
+++ internal/post/client.go
@@ -1,13 +1,17 @@
 package post
 
 import (
+	"context"
 	"github.com/stonecutter/blog-microservices/api/protobuf"
 	"github.com/stonecutter/blog-microservices/internal/pkg/config"
 	"google.golang.org/grpc"
+	"time"
 )
 
 func NewClient(conf *config.Config) (protobuf.PostServiceClient, error) {
-	conn, err := grpc.Dial(conf.Post.Server.Addr, grpc.WithInsecure())
+	ctx, cancel := context.WithTimeout(context.Background(), 5 *time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, conf.Post.Server.Addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
--- internal/user/client.go
+++ internal/user/client.go
@@ -1,13 +1,17 @@
 package user
 
 import (
+	"context"
 	"github.com/stonecutter/blog-microservices/api/protobuf"
 	"github.com/stonecutter/blog-microservices/internal/pkg/config"
 	"google.golang.org/grpc"
+	"time"
 )
 
 func NewClient(conf *config.Config) (protobuf.UserServiceClient, error) {
-	conn, err := grpc.Dial(conf.User.Server.Addr, grpc.WithInsecure())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, conf.User.Server.Addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
