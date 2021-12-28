9b39560acd659853071854db9d17473b99b4b081
jxlwqq <jxlwqq@gmail.com>
2021-12-28 11:01:09 +0800

    add bearer expected scheme in header authorize

--- internal/pkg/interceptor/auth.go
+++ internal/pkg/interceptor/auth.go
@@ -8,6 +8,12 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"strings"
+)
+
+var (
+	expectedScheme  = "bearer"
+	headerAuthorize = "authorization"
 )
 
 func NewAuthInterceptor(logger *log.Logger, jwtManager *jwt.Manager, authMethods map[string]bool) *AuthInterceptor {
@@ -47,11 +53,22 @@ func (i *AuthInterceptor) authorize(ctx context.Context, method string) (*jwt.Us
 	if !ok {
 		return nil, status.Errorf(codes.Unauthenticated, "missing metadata")
 	}
-	values := md["authorization"]
+	values := md[headerAuthorize]
 	if len(values) == 0 {
 		return nil, status.Errorf(codes.Unauthenticated, "authorization token is not provided")
 	}
-	token := values[0]
+
+	splits := strings.SplitN(values[0], " ", 2)
+
+	if len(splits) < 2 {
+		return nil, status.Errorf(codes.Unauthenticated, "Bad authorization string")
+	}
+
+	if !strings.EqualFold(splits[0], expectedScheme) {
+		return nil, status.Errorf(codes.Unauthenticated, "Request unauthenticated with "+expectedScheme)
+	}
+
+	token := splits[1]
 	claims, err := i.jwtManager.Validate(token)
 	if err != nil {
 		return nil, status.Errorf(codes.Unauthenticated, "authorization token is invalid")
